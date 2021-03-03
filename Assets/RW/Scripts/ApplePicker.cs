a92a11c2bdb25c35078b80a59f032e0a9f7b79c8
Sergi PR <t3m1X@users.noreply.github.com>
2021-03-03 23:11:08 +0100

    Now apples disappear when picking them up

---
+++ Assets/RW/Scripts/ApplePicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RayWenderlich.KQClone.Core {
+[RequireComponent(typeof(InteractableObject))]
+public class ApplePicker : MonoBehaviour
+{
+    private InteractableObject m_interactableObject;
+    [SerializeField] private List<SpriteRenderer> MyApples;
+
+    private void Awake() {
+        m_interactableObject = GetComponent<InteractableObject>();
+    }
+
+    public void RemoveApple() {
+        if (MyApples.Count > 0)
+        {
+            MyApples[0].enabled = false;
+            MyApples.RemoveAt(0);
+        }
+        else {
+            m_interactableObject.ChangeState("empty");
+        }
+    }
+}
+
+} //namespace RayWenderlich.KQClone.Utilities
